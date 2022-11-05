477d2007a499a8a86725346e7d733c5e2ca2b1cd
Wasomeno <60634068+Wasomeno@users.noreply.github.com>
2022-11-05 12:56:44 +0700

    Change: small ui changes & show user name in the ui

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,10 @@
-import { useState } from "react";
+import { useContext, useState } from "react";
 import { useRouter } from "next/router";
 import ChartCanvas from "../components/ChartCanvas";
+import { UserContext } from "../context/userContext";
 
 export default function Home() {
+  const userDetails = useContext(UserContext).userDetails;
   const [candidates, setCandidates] = useState([
     "Supardi",
     "Suparman",
@@ -16,25 +18,30 @@ export default function Home() {
 
   return (
     <div className="h-full w-full flex flex-col justify-center items-center">
-      <div className="text-center m-2 mb-4">
-        <h1 className="font-poppins font-medium text-2xl">Voting App</h1>
+      <div className="w-full text-start mb-2">
+        <p className="font-poppins font-medium text-sm ml-4 underline">
+          {userDetails.name}
+        </p>
       </div>
-      <div className="w-full h-5/6 flex flex-wrap justify-around items-start p-2 m-2">
-        <div className="h-3/6 md:h-4/6 w-9/12 md:w-4/12">
-          <div className="text-center m-2 ">
-            <h2 className="font-poppins font-medium text-lg text-center">
-              Votes
-            </h2>
+      <div className="text-center mb-4">
+        <h1 className="font-poppins font-medium text-xl">Voting App</h1>
+      </div>
+      <div className="w-full h-5/6 flex flex-wrap justify-center items-start m-2">
+        <div className="w-full h-2/6 mb-2 flex flex-col items-center">
+          <div className="w-full text-center mb-2">
+            <h2 className="font-poppins font-medium">Current Votes</h2>
           </div>
-          <ChartCanvas />
+          <div className="h-5/6 md:h-4/6 w-9/12 md:w-4/12">
+            <ChartCanvas />
+          </div>
         </div>
-        <div className="h-3/6 md:h-5/6 w-10/12 md:w-6/12">
-          <div className="text-center m-2 ">
-            <h2 className="font-poppins font-medium text-lg text-center">
+        <div className="h-4/6 md:h-5/6 w-10/12 md:w-6/12">
+          <div className="text-center m-2 mb-4">
+            <h2 className="font-poppins font-medium text-base text-center">
               Voting Candidates
             </h2>
           </div>
-          <div className="h-5/6 flex flex-col gap-3 items-center overflow-y-scroll max-h-60 md:max-h-full md:overflow-y-hidden">
+          <div className="h-5/6 flex flex-col gap-3 items-center overflow-y-scroll max-h-96 md:max-h-full md:overflow-y-hidden">
             {candidates.map((candidate, index) => (
               <div
                 key={index}
