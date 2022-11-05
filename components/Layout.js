54ed748195dba5c06996d39e2eaa2cf7277f6b65
Wasomeno <60634068+Wasomeno@users.noreply.github.com>
2022-11-05 14:36:35 +0700

    Add: condition rendering for filter navbar

--- components/Layout.js
+++ components/Layout.js
@@ -1,9 +1,13 @@
+import { useRouter } from "next/router";
 import React, { useState } from "react";
 import { UserContext } from "../context/userContext";
+import { FilterNavbar } from "./Navbar";
 import UserForm from "./UserForm";
 
 const Layout = ({ children }) => {
   const [userDetails, setUserDetails] = useState({ name: "", address: "" });
+  const router = useRouter();
+  const basePath = router.pathname.split("/");
 
   return (
     <main className="flex flex-col justify-start items-center h-screen">
@@ -14,7 +18,10 @@ const Layout = ({ children }) => {
         }}
       >
         {userDetails.name !== "" && userDetails.address !== "" ? (
-          children
+          <>
+            {children}
+            {basePath[1] === "filter" && <FilterNavbar />}
+          </>
         ) : (
           <UserForm />
         )}
